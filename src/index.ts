fe50316962685b85d02330de4cef5759ac89addf
AriqFachry <ariqfachry2611@gmail.com>
2024-01-11 01:17:16 +0700

    tuning bot

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import TelegramBot from "node-telegram-bot-api";
 import OpenAI from "openai";
-import { DEV_RESUME, RESUME_EXPLAIN, SYSTEM_MAIN_TASK } from "./global/prompt";
+import { PROMPT_BOT_CONTEXT, PROMPT_FULL_PROMPT, PROMPT_GREETING, PROMPT_NOT_UNDERSTAND, PROMPT_OUT_CONTEXT, TASK_CLASSIFICATION } from "./global/prompt";
+import { ITaskClassifier } from "./global/type";
+import { ChatCompletionBasic } from "./utils/ai";
 
 require('dotenv').config();
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
-const openai = new OpenAI({
+export const openai = new OpenAI({
     apiKey: process.env.OPENAI_TOKEN
 })
 
@@ -12,13 +14,43 @@ const openai = new OpenAI({
 bot.on('message', async (msg) => {
     // const sender = msg.chat.first_name || "Anonymous";
     bot.sendChatAction(msg.chat.id, "typing")
-    const chatComp = await openai.chat.completions.create({
-        messages: [{ role: 'system', content: `${RESUME_EXPLAIN}` }, { role: 'user', content: msg.text }],
-        model: 'gpt-3.5-turbo',
-        temperature: 0.1
-    });
-    console.log({ msg: msg.text, chatComp, choices: chatComp.choices[0] })
-    bot.sendMessage(msg.chat.id, chatComp.choices[0].message.content)
+    const taskClassifier = await ChatCompletionBasic(TASK_CLASSIFICATION, msg.text)
+    const classifier: ITaskClassifier = JSON.parse(taskClassifier.choices[0].message.content) satisfies ITaskClassifier;
+    console.log({ input: msg.text, classifier: classifier.classification })
+
+    let finalChatCompletion: OpenAI.Chat.Completions.ChatCompletion;
+    let finalResponse: string;
+
+    switch (classifier.classification) {
+        case 'greeting':
+            finalChatCompletion = await ChatCompletionBasic(PROMPT_GREETING, msg.text)
+            finalResponse = finalChatCompletion.choices[0].message.content;
+            break;
+        case 'ask_resume_info':
+            finalChatCompletion = await ChatCompletionBasic(PROMPT_FULL_PROMPT, msg.text)
+            finalResponse = finalChatCompletion.choices[0].message.content;
+            break;
+        case 'asking_out_of_context':
+            finalChatCompletion = await ChatCompletionBasic(PROMPT_OUT_CONTEXT, msg.text)
+            finalResponse = finalChatCompletion.choices[0].message.content;
+            break;
+        case 'asking_about_bot_context':
+            finalChatCompletion = await ChatCompletionBasic(PROMPT_BOT_CONTEXT, msg.text)
+            finalResponse = finalChatCompletion.choices[0].message.content;
+            break;
+        case 'unknown_queries':
+            finalChatCompletion = await ChatCompletionBasic(PROMPT_NOT_UNDERSTAND, msg.text)
+            finalResponse = finalChatCompletion.choices[0].message.content;
+            break;
+        default:
+            finalResponse = "Sorry I'm unable to process your request right now."
+            break;
+    }
+
+    // console.log({ msg: msg.text, chatComp, choices: chatComp.choices[0] })
+    console.log({ usage: finalChatCompletion.usage, response: finalResponse })
+
+    bot.sendMessage(msg.chat.id, finalResponse)
 })
 
 console.log("Project is running");
---
+++ src/utils/ai.ts
@@ -0,0 +1,19 @@
+import { ChatCompletion } from "openai/resources";
+import { PROMPT_GREETING } from "../global/prompt";
+import { openai } from "../index";
+
+
+export const ChatCompletionBasic = async (systemPrompt: string, userQueries: string): Promise<ChatCompletion> => {
+    try {
+        const completion = await openai.chat.completions.create({
+            messages: [{ role: 'system', content: systemPrompt }, { role: 'user', content: userQueries }],
+            model: 'gpt-3.5-turbo',
+            temperature: 0.1
+        });
+        console.log({ response: completion.choices[0].message.content, usage: completion.usage })
+        return completion
+    } catch (error) {
+        console.error(error)
+        Promise.reject(error)
+    }
+}
